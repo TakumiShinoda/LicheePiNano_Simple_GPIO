bf55d0a1cb496dc74db961aa0cab67d84fc8b84e
TakumiShinoda <tshinoda@TakumiShinodanoMacBook-Pro.local>
2020-02-03 16:32:09 +0900

    add pin supports

--- props/props.go
+++ props/props.go
@@ -4,4 +4,4 @@ var Commands = map[string]string{
 	"output": "Control GPIO outputs.",
 	"pins":   "Check supported pins.",
 }
-var LicheePins = map[string]int{"D7": 138, "D6": 137}
+var LicheePins = map[string]int{"D7": 138, "D6": 137, "D5": 136, "D4": 135, "D3": 134, "D2": 133, "D1": 132, "D0": 131}
